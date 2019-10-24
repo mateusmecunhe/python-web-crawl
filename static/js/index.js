5f6e64f3ea990fde1480172d1f2f0211891429a3
Mateus Faria <mateusmecunhe@gmail.com>
2019-10-24 16:18:10 -0300

    structuring javascript

--- static/js/index.js
+++ static/js/index.js
@@ -8,6 +8,7 @@ buttons.forEach(function(b){
     let btn_id_int = parseInt(btn_id_str, 10)
   
     b.addEventListener('click', function(){
+        find_areas(modals[btn_id_int])
         modals[btn_id_int].classList.remove('hide')
     })
 })
@@ -19,3 +20,28 @@ modal_closing_btns.forEach(function(b){
         })
     })
 })
+
+function find_areas(element){
+    modals.forEach(function(m){
+        let str = m.innerText
+        let matches = str.matchAll(/M²/)
+        for (const match of matches) {
+            index = match.index
+            console.log(match.index);
+
+            console.log(str[index])
+            index++
+            console.log(str[index])
+            index--
+            let contador_de_espacos = 0
+            while (contador_de_espacos < 2){
+                if(str[index] == " "){
+                    contador_de_espacos++
+                }
+                console.log(str[index])
+                index--
+
+            }
+          }
+    })
+}
